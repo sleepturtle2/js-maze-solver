4baa129b5a7ae85883d382e6da36cef750acc576
sleepturtle2 <sayantan.mukh98@gmail.com>
2021-07-15 15:08:46 +0530

    working version 1

---
+++ backtracking_algo.js
@@ -0,0 +1,119 @@
+var grid =
+  [
+  [1, 0, 0, 0],
+  [1, 1, 0, 1],
+  [0, 1, 0, 0],
+  [1, 1, 1, 1]
+  ];
+var row = grid.length;
+var col = grid[0].length;
+var end_x = row-1;
+var end_y = col - 1;
+function isSafe(x, y) {
+  if (x >= 0 && x < row && y >= 0 && y < col  && grid[x][y] == 1) return true;
+  return false;
+}
+
+function print(a) {
+  for (i = 0; i < a.length; i++)
+  {
+      console.log(...a[i]);
+    }
+}
+
+function backtrack(sol) {
+  let x = end_x;
+  let y = end_y;
+
+  sol[x][y] = 2; //marks the end of the path
+  //console.log(sol[x][y]);
+  while (x != 0 && y != 0) {
+    //check up
+    if (isSafe(x - 1, y) && sol[x - 1][y] == 1) {
+      sol[x-1][y] = 0;
+      x = x - 1;
+      console.log("up");
+      continue;
+    }
+    //check down
+    else if (isSafe(x + 1, y) && sol[x + 1][y] == 1) {
+      sol[x + 1][y] = 0;
+      x = x + 1;
+      console.log("down");
+      continue;
+    }
+    //check right
+    else if (isSafe(x, y + 1) && sol[x][y + 1] == 1) {
+      sol[x][y + 1] = 0;
+      y = y + 1;
+      console.log("right");
+      continue;
+    }
+      //check left
+    else if (isSafe(x, y - 1) && sol[x][y - 1] == 1) {
+      sol[x][y - 1] = 0;
+      y = y - 1;
+      console.log("left");
+      continue;
+    }
+  }
+}
+
+function solveMazeUtil(x, y, sol) {
+  if (x == row - 1 && y == col - 1 && grid[x][y] == 1) {
+    sol[x][y] = 1;
+    end_x = row - 1;
+    end_y = col - 1;
+    return true;
+  }
+
+  if (isSafe(x, y) == true) {
+
+    if (sol[x][y] == 1)
+      return false;
+
+    sol[x][y] = 1;
+
+    if (solveMazeUtil(x + 1, y, sol) == true)
+      return true;
+
+    if (solveMazeUtil(x - 1, y, sol) == true) return true;
+
+    if (solveMazeUtil(x, y - 1, sol) == true) return true;
+
+    if (solveMazeUtil(x, y + 1, sol) == true) return true;
+
+    sol[x][y] = 0;
+    return false;
+  }
+
+  return false;
+}
+
+function solveMaze() {
+  let sol = new Array(grid.length);
+  let output;
+
+  //create solution maze
+  for (i = 0; i < sol.length; i++)
+    sol[i] = new Array(grid[0].length);
+  //initialize solution maze
+  for (i = 0; i < grid.length; i++)
+    for (j = 0; j < grid[0].length; j++)
+      sol[i][j] = 0;
+
+
+
+  if (solveMazeUtil(0, 0, sol) == false)
+    ouptut = "Solution does not exist";
+  else {
+    output = "The solution path has been displayed above"
+    print(sol);
+    backtrack(sol);
+  }
+
+  console.log(output);
+
+}
+
+solveMaze();
--- maze.js
+++ maze.js
@@ -5,11 +5,21 @@ var HEIGHT = 800;
 
 var tileW = 20;
 var tileH = 20;
-var tileRowCount = 2;
-var tileColumnCount = 40;
+var tileRowCount = 3;
+var tileColumnCount = 5;
+
+var boundX = 0;
+var boundY = 0;
 
 var tiles = [];
-for ( c = 0; c < tileColumnCount; c++){
+
+function solveMaze() {
+
+}
+
+
+function reset() {
+  for ( c = 0; c < tileColumnCount; c++){
   tiles[c] = [];
   for (r = 0; r < tileRowCount; r++){
     tiles[c][r] = {x:c*(tileW+3), y:r*(tileH+3) ,state: 'e'} //e = empty
@@ -19,7 +29,24 @@ for ( c = 0; c < tileColumnCount; c++){
 tiles[0][0].state = 's'; //start
 tiles[tileColumnCount-1][tileRowCount-1].state='f';
 
-function rect(x, y, w, h) {
+  output.innerHtml = '';
+}
+
+function rect(x, y, w, h, state) {
+  if (state == 's') {
+    context.fillStyle = '#00FF00';
+  }
+  else if (state == 'f') {
+    context.fillStyle = '#FF0000'
+  }
+  else if (state == 'w') {
+    context.fillStyle = '#0000FF';
+  }
+  else if (state == 'e') {
+    context.fillStyle = '#AAAAAA';
+    }
+
+
   context.beginPath();
   context.rect(x, y, w, h);
   context.closePath();
@@ -32,18 +59,75 @@ function clear() {
 
 function draw() {
   clear();
-  context.fillStyle = '#FF0000';
+
   for (c = 0; c < tileColumnCount; c++)
     for (r = 0; r < tileRowCount; r++){
-      rect(tiles[c][r].x, tiles[c][r].y, tileW, tileH);
+      rect(tiles[c][r].x, tiles[c][r].y, tileW, tileH, tiles[c][r].state);
     }
 }
 
 function init() {
+  output = document.getElementById('outcome');
+  reset();
   canvas = document.getElementById("myCanvas");
   context = canvas.getContext("2d");
-  return setInterval(draw(), 10);
+  //draw();
+
+  return setInterval(draw, 10);
+}
+
+function myMove(event) {
+  x = event.pageX - canvas.offsetLeft;
+  y = event.pageY - canvas.offsetTop;
+  console.log(x, y);
+  for (c = 0; c < tileColumnCount; c++)
+    for (r = 0; r < tileRowCount; r++){
+      //if x and y is in range of a particular cell
+      if (c * (tileW + 3) < x && x < c * (tileW + 3) + tileW && r * (tileH + 3) < y && y < r * (tileH + 3) + tileH) {
+        if (tiles[c][r].state == 'e'&& (c!=boundX || r != boundY)) {
+          tiles[c][r].state = 'w'; //wall
+          boundX = c;
+          boundY = r;
+        }
+
+        else if (tiles[c][r].state == 'w' && (c!=boundX || r != boundY)) {
+          tiles[c][r].state = 'e';
+          boundX = c;
+          boundY = r;
+        }
+      }
+    }
+}
+
+function myDown(event) {
+  canvas.onmousemove = myMove;
+  x = event.pageX - canvas.offsetLeft;
+  y = event.pageY - canvas.offsetTop;
+  console.log(x, y);
+  for (c = 0; c < tileColumnCount; c++)
+    for (r = 0; r < tileRowCount; r++){
+      //if x and y is in range of a particular cell
+      if (c * (tileW + 3) < x && x < c * (tileW + 3) + tileW && r * (tileH + 3) < y && y < r * (tileH + 3) + tileH) {
+        if (tiles[c][r].state == 'e' ) {
+          tiles[c][r].state = 'w'; //wall
+          boundX = c;
+          boundY = r;
+        }
+
+        else if (tiles[c][r].state == 'w') {
+          tiles[c][r].state = 'e';
+          boundX = c;
+          boundY = r;
+        }
+      }
+    }
+}
+
+function myUp() {
+  canvas.onmousemove = null;
 }
 
 init();
 
+canvas.onmousedown = myDown;
+canvas.onmouseup = myUp;
