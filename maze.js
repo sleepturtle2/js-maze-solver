72027bab15d3d4a06d1052c21f7d8edf6bcd7f5d
sleepturtle2 <sayantan.mukh98@gmail.com>
2021-07-15 20:57:11 +0530

    working version 3.0

--- maze.js
+++ maze.js
@@ -1,130 +1,191 @@
 var canvas;
 var context;
-var WIDTH = 1200;
-var HEIGHT = 600;
+var output = document.getElementById("outcome");
+const WIDTH = 1200;
+const HEIGHT = 600;
 
 var tileW = 20;
 var tileH = 20;
-var tileRowCount = 3;
-var tileColumnCount = 5;
-var end_x = tileColumnCount - 1; // x index of finish
-var end_y = tileRowCount - 1; //y index of finish
+var tileRowCount = 20;
+var tileColumnCount = 25;
+var tiles;
 
 var boundX = 0;
 var boundY = 0;
 
-var tiles = [];
+var end_x = tileRowCount - 1;
+var end_y = tileColumnCount - 1;
 
+var steps = 0;
+
+//PATH FINDING SOLUTION
 function isSafe(x, y) {
-  if (x >= 0 && x < tileColumnCount && y >= 0 && y < tileRowCount && (tiles[x][y].state != 'w'))
+  if (x >= 0 && x < tileRowCount && y >= 0 && y < tileColumnCount && (tiles[x][y].state != 'w'))
     return true;
 
   return false;
 }
 
 function print(a) {
+  var str;
   for (i = 0; i < a.length; i++){
     for (j = 0; j < a[0].length; j++)
-      document.write(" " + a[i][j].state + " ");
-    document.write("<br>");
+      str += a[i][j].state + " ";
+    console.log(str);
+    str = "";
   }
 }
 
-function solveMazeUtil(x, y, tiles) {
-  if (x == end_x && y == end_y) {
-    //tiles[x][y].state = 'x';
+function solveMazeUtil(x, y, sol) {
+  if (x == end_x && y == end_y && tiles[x][y].state == 'f') {
+    sol[x][y] = 1;
+    steps++;
     return true;
   }
 
-  if (isSafe(x, y) == true) {
+  if (isSafe(x, y) === true) {
+
+     // Check if the current block is already part of solution path.
+    if (sol[x][y] ===  1) return false;
     console.log(x, y);
-    if (tiles[x][y].state == 'v' || tiles[x][y].state == 'i') return false;
+     // mark x, y as part of solution path
+    sol[x][y] = 1;
+    if(tiles[x][y].state != 's')
+      tiles[x][y].state = 'x';
+    steps++;
 
-    tiles[x][y].state = 'v';
+    //right
+    if (solveMazeUtil(x, y + 1, sol) === true) return true;
+    //down
+    else if (solveMazeUtil(x + 1, y, sol) === true) return true;
+    //left
+    else if (solveMazeUtil(x - 1, y, sol) === true) return true;
+    //up
+    else if (solveMazeUtil(x, y - 1, sol) === true) return true;
 
-    if (solveMazeUtil(x + 1, y, tiles) == true)
-      return true;
-    else if (solveMazeUtil(x - 1, y, tiles) == true) return true;
 
-    else if (solveMazeUtil(x, y - 1, tiles) == true) return true;
-
-    else if (solveMazeUtil(x, y + 1, tiles) == true) return true;
-
-    tiles[x][y].state = 'i'; // visited but incorrect, ie cannot be part of solution
+    sol[x][y] = 0; //if all cases are false, this cell cannot be part of path
+    tiles[x][y].state = 'e';
+    steps--;
     return false;
   }
   return false;
 }
 
-function backtrack(sol) {
-  x = end_x; y = end_y;
-  //tiles[x][y].state = 'x';
-
-  tiles[x][y].state = 'f';
-
-  while (x != 0 && y != 0) {
-    //check up
-    if (isSafe(x - 1, y) && tiles[x - 1][y].state == 'v') {
-      tiles[x - 1][y].state = 'x';
-      x = x - 1;
-      continue;
-    }
-    //check down
-    else if (isSafe(x + 1, y) && tiles[x + 1][y].state == 'v') {
-      tiles[x + 1][y].state = 'x';
-      x = x + 1;
-      continue;
-    }
-    //check right
-    else if (isSafe(x, y + 1) && tiles[x][y + 1].state == 'v') {
-      tiles[x][y + 1].state = 'x';
-      y = y + 1;
-      continue;
-    }
-    else if (isSafe(x, y - 1) && tiles[x][y - 1].state == 'v') {
-      tiles[x][y - 1].state = 'x';
-      y = y - 1;
-      continue;
-    }
-
-  }
-}
-
 function solveMaze() {
-  //console.log(tiles.length, tiles[0].length);
-
   let sol = new Array(tiles.length);
 
-  //create solution maze
+   //create solution maze
   for (i = 0; i < sol.length; i++)
     sol[i] = new Array(tiles[0].length);
   for (i = 0; i < sol.length; i++)
     for (j = 0; j < sol[0].length; j++)
       sol[i][j] = 0;
-   //console.log(sol.length, sol[0].length);
+  //console.log(sol.length, sol[0].length);
 
-  if (solveMazeUtil(0, 0, tiles) == false)
-    console.log("no output");
+  if (solveMazeUtil(0, 0, sol) == false)
+    output.innerHTML = "No solution possible!";
   else {
-    //print(tiles);
-    backtrack(tiles);
-  }
+    /*
+    // print sol
+    console.log("printing solution");
 
+    var str="";
+    for (i = 0; i < sol.length; i++)
+    {
+      for (j = 0; j < sol[0].length; j++)
+        str += sol[i][j];
+      console.log(str);
+      str = "";
+      }
+      */
+    output.innerHTML = "Shortest path in " + steps + " steps!";
+  }
 }
 
 
-function reset() {
-  for ( c = 0; c < tileColumnCount; c++){
-  tiles[c] = [];
-  for (r = 0; r < tileRowCount; r++){
-    tiles[c][r] = {x:c*(tileW+3), y:r*(tileH+3) ,state: 'e'} //e = empty
+
+//BUILD WALL FUNCTIONS
+
+function Move(event) {
+
+  x = event.pageX - canvas.offsetLeft;
+  y = event.pageY - canvas.offsetTop;
+  //console.log(x, y);
+  for (r = 0; r < tileRowCount; r++)
+    for (c = 0; c < tileColumnCount; c++){
+      //if x and y is in range of a particular cell
+      if (c * (tileW + 3) < x && x < c * (tileW + 3) + tileW && r * (tileH + 3) < y && y < r * (tileH + 3) + tileH) {
+        console.log(r, c);
+        if (tiles[r][c].state == 'e'&& (r!=boundX || c != boundY)) {
+          tiles[r][c].state = 'w'; //wall
+          boundX = r;
+          boundY = c;
+        }
+
+        else if (tiles[r][c].state == 'w' && (r!=boundX || c != boundY)) {
+          tiles[r][c].state = 'e';
+          boundX = r;
+          boundY = c;
+        }
+      }
     }
-  }
+}
 
-  tiles[0][0].state = 's'; //start
-  tiles[tileColumnCount-1][tileRowCount-1].state='f';
+function Down(event) {
+  canvas.onmousemove = Move;
+  x = event.pageX - canvas.offsetLeft;
+  y = event.pageY - canvas.offsetTop;
 
-  output.innerHtml = '';
+  //console.log(x, y);
+
+  for (r = 0; r < tileRowCount; r++)
+    for (c = 0; c < tileColumnCount; c++){
+      //if x and y is in range of a particular cell
+      if (c * (tileW + 3) < x && x < c * (tileW + 3) + tileW && r * (tileH + 3) < y && y < r * (tileH + 3) + tileH) {
+        console.log(r, c);
+
+        if (tiles[r][c].state == 'e' ) {
+          tiles[r][c].state = 'w'; //wall
+          boundX = r;
+          boundY = c;
+        }
+
+        else if (tiles[r][c].state == 'w') {
+          tiles[r][c].state = 'e';
+          boundX = r;
+          boundY = c;
+        }
+      }
+    }
+}
+
+function Up() {
+  canvas.onmousemove = null;
+}
+// INIT FUNCTIONS
+function clear() {
+  context.clearRect(0, 0, WIDTH, HEIGHT);
+}
+
+function reset() {
+  tiles = new Array(tileRowCount);
+  for (i = 0; i < tiles.length; i++)
+    tiles[i] = new Array(tileColumnCount);
+
+  for (r = 0; r < tileRowCount; r++)
+    for (c = 0; c < tileColumnCount; c++)
+    {
+      tiles[r][c] = { x: c * (tileW + 3), y: r * (tileH + 3), state: 'e' };
+      //console.log(tiles[r][c]);
+    }
+
+
+  tiles[0][0].state = 's';
+  tiles[tileRowCount - 1][tileColumnCount - 1].state = 'f';
+  output.innerHTML = "";
+
+  return tiles;
 }
 
 function rect(x, y, w, h, state) {
@@ -143,89 +204,27 @@ function rect(x, y, w, h, state) {
   else if (state == 'x') {
     context.fillStyle = '#000000';
   }
-
-
   context.beginPath();
   context.rect(x, y, w, h);
   context.closePath();
   context.fill();
 }
 
-function clear() {
-  context.clearRect(0, 0, WIDTH, HEIGHT);
-}
-
 function draw() {
   clear();
 
-  for (c = 0; c < tileColumnCount; c++)
-    for (r = 0; r < tileRowCount; r++){
-      rect(tiles[c][r].x, tiles[c][r].y, tileW, tileH, tiles[c][r].state);
-    }
+  for (r = 0; r < tileRowCount; r++)
+    for (c = 0; c < tileColumnCount; c++)
+      rect(tiles[r][c].x, tiles[r][c].y, tileW, tileH, tiles[r][c].state);
 }
 
 function init() {
-  output = document.getElementById('outcome');
-  reset();
-  canvas = document.getElementById("myCanvas");
-  context = canvas.getContext("2d");
-  //draw();
-
+  tiles = reset();
+  canvas = document.getElementById('myCanvas');
+  context = canvas.getContext('2d');
   return setInterval(draw, 10);
 }
 
-function myMove(event) {
-  x = event.pageX - canvas.offsetLeft;
-  y = event.pageY - canvas.offsetTop;
-  console.log(x, y);
-  for (c = 0; c < tileColumnCount; c++)
-    for (r = 0; r < tileRowCount; r++){
-      //if x and y is in range of a particular cell
-      if (c * (tileW + 3) < x && x < c * (tileW + 3) + tileW && r * (tileH + 3) < y && y < r * (tileH + 3) + tileH) {
-        if (tiles[c][r].state == 'e'&& (c!=boundX || r != boundY)) {
-          tiles[c][r].state = 'w'; //wall
-          boundX = c;
-          boundY = r;
-        }
-
-        else if (tiles[c][r].state == 'w' && (c!=boundX || r != boundY)) {
-          tiles[c][r].state = 'e';
-          boundX = c;
-          boundY = r;
-        }
-      }
-    }
-}
-
-function myDown(event) {
-  canvas.onmousemove = myMove;
-  x = event.pageX - canvas.offsetLeft;
-  y = event.pageY - canvas.offsetTop;
-  console.log(x, y);
-  for (c = 0; c < tileColumnCount; c++)
-    for (r = 0; r < tileRowCount; r++){
-      //if x and y is in range of a particular cell
-      if (c * (tileW + 3) < x && x < c * (tileW + 3) + tileW && r * (tileH + 3) < y && y < r * (tileH + 3) + tileH) {
-        if (tiles[c][r].state == 'e' ) {
-          tiles[c][r].state = 'w'; //wall
-          boundX = c;
-          boundY = r;
-        }
-
-        else if (tiles[c][r].state == 'w') {
-          tiles[c][r].state = 'e';
-          boundX = c;
-          boundY = r;
-        }
-      }
-    }
-}
-
-function myUp() {
-  canvas.onmousemove = null;
-}
-
 init();
-
-canvas.onmousedown = myDown;
-canvas.onmouseup = myUp;
+canvas.onmousedown = Down;
+canvas.onmouseup = Up;
